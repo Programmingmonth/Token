54b9709cac77eba44f775626a5deb543ad9d84eb
Williamfox2012 <141017859+Programmingmonth@users.noreply.github.com>
2024-05-20 18:32:48 +0330

    first commit

--- Token/web/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Tokens(models.Model):
-    char = models.CharField(max_length=255)
-    amount = models.BigIntegerField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
